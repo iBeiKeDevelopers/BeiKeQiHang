76814fd6d8e4f34a89c2e0c6acfe2ab7dce505e7
vincenteliang <vincenteljh@gmail.com>
2020-06-24 21:23:47 +0800

    3.0.2
    切换页面顺序

---
+++ pages/strategy/strategy.js
@@ -0,0 +1,27 @@
+Page({
+
+  data: {
+    screenHeight: 1205
+  },
+  onLoad: function () {
+    wx.showShareMenu({
+      withShareTicket: true
+    })
+    var height = 1205
+    wx.getSystemInfo({
+      success: function (res) {
+        console.log(res.screenHeight)
+        height = (res.screenHeight * 2) - 130
+      },
+    })
+    console.log(height)
+    this.setData({
+      screenHeight: height
+    });
+  },
+  goTo: function (e) {
+    wx.navigateTo({
+      url: e.currentTarget.dataset.target + '/' + e.currentTarget.dataset.target,
+    })
+  }
+})
